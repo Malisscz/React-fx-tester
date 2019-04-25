709da48d89542a410e18f970703b293fc71861c2
Jirka Adam <jadam@trask.cz>
2019-04-25 14:25:12 +0200

    graf jako komponenta

--- src/components/trader/Trader.js
+++ src/components/trader/Trader.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Chart} from "../chart/Chart";
 
 export class Trader extends React.Component {
     render() {
@@ -9,16 +10,7 @@ export class Trader extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-8 chart__cont">
-                        <iframe
-                            id="tradingview_bde47"
-                            title="Fx chart"
-                            src="https://s.tradingview.com/dailyfx/widgetembed/?frameElementId=tradingview_bde47&amp;symbol=FX_IDC%3AEURUSD&amp;interval=D&amp;hidesidetoolbar=0&amp;symboledit=1&amp;saveimage=1&amp;toolbarbg=f4f7f9&amp;studies=%5B%5D&amp;hideideas=1&amp;theme=White&amp;timezone=exchange&amp;showpopupbutton=1&amp;studies_overrides=%7B%7D&amp;overrides=%7B%7D&amp;enabled_features=%5B%5D&amp;disabled_features=%5B%5D&amp;showpopupbutton=1&amp;locale=en&amp;utm_source=www.dailyfx.com&amp;utm_medium=widget&amp;utm_campaign=chart&amp;utm_term=FX_IDC%3AEURUSD"
-                            className="chart__iframe"
-                            frameBorder="0"
-                            allowTransparency="true"
-                            scrolling="no"
-                            allowFullScreen="">
-                        </iframe>
+                        <Chart />
                     </div>
                     <div className="col-4">
                         <div className="container">
